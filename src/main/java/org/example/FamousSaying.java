8112434a4a599232d6f7da14b19a3ab8e6576732
yoonjoonseok <yjsygm@naver.com>
2023-03-02 11:14:04 +0900

    리팩토링1

---
+++ src/main/java/org/example/FamousSaying.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class FamousSaying {
+    private long id;
+    private String famousSaying;
+    private String author;
+
+    public FamousSaying(long id, String famousSaying, String author) {
+        this.id = id;
+        this.famousSaying = famousSaying;
+        this.author = author;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFamousSaying() {
+        return famousSaying;
+    }
+
+    public void setFamousSaying(String famousSaying) {
+        this.famousSaying = famousSaying;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}

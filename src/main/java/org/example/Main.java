6f4247e4e0d61428c138bca7fd7a7e95c5e3fcef
yoonjoonseok <yjsygm@naver.com>
2023-03-02 09:38:54 +0900

    7단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -24,14 +24,18 @@ public class Main {
                 map.put(id,fs);
                 System.out.printf("%d번 명언이 등록되었습니다\n",id);
             } else if(input.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언\n----------------------");
                 for(long key:map.keySet())
                     System.out.printf("%d / %s / %s\n",key,map.get(key).getFamousSaying(),map.get(key).getAuthor());
             }else if(input.matches("삭제\\?id=[0-9]+")){
                 long id2 = Long.parseLong(input.replace("삭제?id=",""));
-                map.remove(id2);
-                System.out.printf("%d번 명언이 삭제되었습니다\n",id2);
-            }
-            else if(input.equals("종료"))
+                if(map.containsKey(id2)){
+                    map.remove(id2);
+                    System.out.printf("%d번 명언이 삭제되었습니다\n",id2);
+                }
+                else
+                    System.out.printf("%d번 명언은 존재하지 않습니다.\n",id2);
+            } else if(input.equals("종료"))
                 break;
             else{
                 System.out.println("잘못된 명령입니다");
